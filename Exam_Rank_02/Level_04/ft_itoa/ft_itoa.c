bb4e8b1dbcfd409b03d05b65b3b09e46c5ac4528
ccg-v <ccg-v@hotmail.com>
2023-01-04 22:58:48 +0100

    Extracting main

--- Exam_Rank_02/Level_04/ft_itoa/ft_itoa.c
+++ Exam_Rank_02/Level_04/ft_itoa/ft_itoa.c
@@ -55,33 +55,3 @@ char	*ft_itoa(int nbr)
 	}
 	return(str);
 }
-
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int	main(int argc, char **argv)
-{
-	char 	*str;
-	int		len;
-	int		nbr;
-	int		i;
-
-	if (argc == 2)
-	{
-		nbr = atoi(argv[1]);
-		len = ft_nbrlen(nbr);
-		str = ft_itoa(nbr);
-		i = 0;
-		while (i < len)
-		{
-			printf("str[%d] = %c\n", i, str[i]);
-			i++;
-		}
-		printf("str is %s\n", str); // Anado esta linea porque no se por que el
-	}								// bucle anterior no imprime si str es cero
-	else
-		printf("Wrong arguments!\n");
-	return(0);
-}
-
