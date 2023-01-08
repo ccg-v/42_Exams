2d5e20c9ee6fbb0e6631b4a0bdeba7546d41a354
ccg-v <ccg-v@hotmail.com>
2023-01-08 22:58:15 +0100

    Uploading paramsum

---
+++ Exam_Rank_02/Level_03/paramsum/paramsum.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   paramsum.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccarrace <ccarrace@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/05 20:44:41 by ccarrace          #+#    #+#             */
+/*   Updated: 2022/12/05 21:15:30 by ccarrace         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnbr(int nbr)
+{
+	char	c;
+
+	if (nbr >= 9)
+		ft_putnbr(nbr / 10);
+	c = (nbr % 10) + '0';
+	write(1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+
+	if (argc > 1)
+		ft_putnbr(argc - 1);
+	else
+		write(1, "0", 1);
+	write(1, "\n", 1);
+}
