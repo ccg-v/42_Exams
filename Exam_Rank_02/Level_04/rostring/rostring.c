7019f7186ac65fe841c4483ab85d872f2b66e642
ccg-v <ccg-v@hotmail.com>
2023-01-18 19:17:20 +0100

    Fixed: didn't ignore spaces at the end

--- Exam_Rank_02/Level_04/rostring/rostring.c
+++ Exam_Rank_02/Level_04/rostring/rostring.c
@@ -6,7 +6,7 @@
 /*   By: ccarrace <ccarrace@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 20:55:52 by ccarrace          #+#    #+#             */
-/*   Updated: 2022/12/15 20:56:04 by ccarrace         ###   ########.fr       */
+/*   Updated: 2023/01/18 19:16:06 by ccarrace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,39 @@ int	ft_isblank(char c)
 	return(0);
 }
 
-int	main(int argc, char **argv)
+int	ft_strlen(char *s)
 {
 	int	i;
-	int	start;
-	int	end;
-	int	flag;
-	char	*s;
-
 
 	i = 0;
+	while (*s++)
+		i++;
+	return (i);
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		start;
+	int		end;
+	int		flag;
+	char	*s;
+	int		len;
+
 	if (argc > 1)
 	{
 		s = argv[1];
-		// skip previous spaces
+		i = 0;
+		flag = 0;
+		len = ft_strlen(s) - 1;
+		// ignore spaces at the end
+		while (ft_isblank(s[len]))
+			len--;
+		// skip spaces at the beginning
 		while(ft_isblank(s[i]) == 1 && s[i] != '\0')
 			i++;
 		start = i;
-		// skip first word
+		// skip first word, storing its start and end positions
 		while(ft_isblank(s[i]) == 0 && s[i] != '\0')
 			i++;
 		end = i;
@@ -45,7 +60,7 @@ int	main(int argc, char **argv)
 			i++;
 		// display from second to last word, but printing ONLY ONE space
 		// between words	
-		while(s[i] != '\0')
+		while(i <= len)
 		{
 			if (ft_isblank(s[i]) == 0)
 			{
@@ -58,11 +73,11 @@ int	main(int argc, char **argv)
 		}
 		if (flag == 1)
 			write(1, " ", 1);	// <--- ... to display a space before the first word
-						// 	when printing it at the end of the string ONLY 
-						// 	IF A PREVIOUS WORD HAS ALREADY BEEN DISPLAYED
-						//      (if the string has only one word, there's no 
-						//      previous word thus we don't need a separation
-						//      delimiter)
+								// 	when printing it at the end of the string ONLY 
+								// 	IF A PREVIOUS WORD HAS ALREADY BEEN DISPLAYED
+								//  (if the string has only one word, there's no 
+								//  previous word thus we don't need a separation
+								//  delimiter)
 
 		// display first word at the end of the string
 		while(start < end)
