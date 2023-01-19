d52487299da558b2b31fab82698ed6e87c1ef161
ccg-v <ccg-v@hotmail.com>
2023-01-19 23:27:29 +0100

    Updating main.c

--- Exam_Rank_02/Level_03/ft_atoi_base/main.c
+++ Exam_Rank_02/Level_03/ft_atoi_base/main.c
@@ -2,31 +2,39 @@
 #include <stdlib.h>
 
 int	ft_atoi_base(char *str, int str_base);
-int	ft_isvalid(char c, int str_base);
 
+int	main(void)
+{
+	printf("---------------------------------------------------------------------------------\n");
+	printf("'13268!' in base 16 is 78440. Your function returns:\n");
+	printf("-----> %d\n",ft_atoi_base("13268!", 16)); 
+	printf("---------------------------------------------------------------------------------\n");
+	printf("'  	-13268!' in base 11 is -18950. Your function returns:\n");
+	printf("-----> %d\n",ft_atoi_base("  	-13268", 11)); 
+	printf("---------------------------------------------------------------------------------\n");
+	printf("'  Decaffeinated coffee is awful' in base 16 is 233615358. Your function returns:\n");
+	printf("-----> %d\n", ft_atoi_base("  Decaffeinated coffee is awful", 16));
+	printf("---------------------------------------------------------------------------------\n");
+}
+
+/* Main to test function sending your own numbers
+ * To check if conversion is right, follow the link below:
+ * https://www.rapidtables.com/convert/number/base-converter.html
+*/
+/*
 int	main(int argc, char **argv)
 {
 	char	*nbr;
 	int		str_base;
-	int		i;
 
 	if (argc == 3)
 	{
 		nbr = argv[1];
 		str_base = atoi(argv[2]);
-		i = 0;
-		while (nbr[i])
-		{
-			if (!ft_isvalid(nbr[i], str_base))
-			{
-				printf("%s is not a valid base %d number!\n", nbr, str_base);
-				return (0);
-			}
-			i++;
-		}
 		printf("%s in base %d is %d in base 10\n", nbr, str_base, ft_atoi_base(nbr, str_base));
 	}
 	else
 		printf("This function needs two arguments: a number and its base\n");
 	return (0);
 }
+*/
